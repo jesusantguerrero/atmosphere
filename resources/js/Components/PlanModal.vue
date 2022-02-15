33101e0d05796e5b4b28d26f3f7af76b3fafdb34
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-02-15 08:58:42 -0400

    fix: search width

--- resources/js/Components/PlanModal.vue
+++ resources/js/Components/PlanModal.vue
@@ -10,10 +10,10 @@
 
         <div class="px-4 pb-4 bg-white sm:p-6 sm:pb-4">
             <div class="sm:flex sm:items-start">
-                <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <div class="w-full text-center sm:mt-0 sm:ml-4 sm:text-left">
                     <slot name="content">
-                        <div>
-                            <at-input v-model="searchText" placeholder="Search meal by name" />
+                        <div class="w-full">
+                            <at-input v-model="searchText" placeholder="Search meal by name" type="search" class="w-full" />
                             <meal
                                 class="overflow-auto h-96"
                                 :meals="filteredMeals"
