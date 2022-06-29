00234f41b658c1bd67d58ac8f3f39685c8f7cf69
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-06-29 16:27:55 -0400

    chore: update inertia and migrate to vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,23 +1,23 @@
-require('./bootstrap');
+import './bootstrap';
+import '../css/app.css';
 
 // Import modules...
 import { createApp, h } from 'vue';
-import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue3';
+import { createInertiaApp, Link } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
 import "atmosphere-ui/style.css"
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-const el = document.getElementById('app');
-
-
-createApp({
-    render: () =>
-        h(InertiaApp, {
-            initialPage: JSON.parse(el.dataset.page),
-            resolveComponent: (name) => require(`./Pages/${name}`).default,
-        }),
-})
-    .mixin({ methods: { route } })
-    .use(InertiaPlugin)
-    .mount(el);
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, app, props, plugin }) {
+        createApp({ render: () => h(app, props)})
+        .use(plugin)
+        .use(ZiggyVue, Ziggy)
+        .mount(el);
+    }
+});
 
 InertiaProgress.init({ color: '#4B5563' });
