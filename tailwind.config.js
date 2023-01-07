e94bc83de34dcebc93712c11c6b5ae96a38cffda
Jesus Guerrero <jesusant.guerrero@gmail.com>
2023-01-07 18:22:28 -0400

    style: add ripple

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,15 @@ module.exports = {
             },
             colors: {
                 ...themes.defaultLight
+            },
+            keyframes: {
+                ripple: {
+                    '0%': { width: '0px', height: '0px', opacity: 0.5},
+                    '100%': { width: '500px', height: '500px', opacity: 0}
+                }
+            },
+            animation: {
+                ripple: 'ripple 1s linear infinite'
             }
         },
     },
