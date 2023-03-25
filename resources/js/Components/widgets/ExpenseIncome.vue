50773741354e8ea4c129a60d1860d8f3bef4f69c
Jesus Guerrero <jesusant.guerrero@gmail.com>
2023-03-25 16:01:59 -0400

    fix: improve trends header

--- resources/js/Components/widgets/ExpenseIncome.vue
+++ resources/js/Components/widgets/ExpenseIncome.vue
@@ -1,57 +1,50 @@
+<script lang="ts" setup>
+import SectionTitle from "@/Components/atoms/SectionTitle.vue";
+import MoneyPresenter from "../molecules/MoneyPresenter.vue";
+
+interface IFooterStat {
+    label: string;
+    value: number;
+    class?: string;
+}
+
+withDefaults(defineProps<{
+    title: string;
+    subtitle?: string;
+    value: number;
+    footerStats?: IFooterStat[];
+    footer?: string
+}>(), {
+    title: 'Month Revenue',
+})
+</script>
+
+
 <template>
   <section class="space-y-2 border border-transparent rounded bg-base-lvl-3 p-4" :class="[cardShadow]">
     <header class="flex justify-between items-center">
-      <SectionTitle>Month Revenue</SectionTitle>
-      <span class="text-secondary font-bold text-sm">Oct</span>
+      <SectionTitle>{{ title }}</SectionTitle>
+      <span class="text-secondary font-bold text-sm">
+        {{  subtitle }}
+    </span>
     </header>
     <article class="flex space-x-4">
       <section>
         <h4 class="text-secondary font-bold text-3xl relative">
-            <MoneyPresenter
-                :value="income + expenses"
-            />
+            <MoneyPresenter :value="value" />
         </h4>
         <p class="text-body-1/80 mt-4 space-x-4">
-            <span>
-                Expenses
+            <span v-for="stat in footerStats">
+                {{ stat.label}}:
                 <MoneyPresenter
-                    class="text-error"
-                    :value="expenses"
-                />
-            </span>
-            <span>
-                Income:
-                <MoneyPresenter
-                    class="text-success"
-                    :value="income"
+                    :class="stat.class"
+                    :value="stat.value"
                 />
             </span>
         </p>
+        <p class="mt-0 text-body-1"> {{ footer }}</p>
       </section>
     </article>
   </section>
 </template>
 
-<script setup>
-import SectionTitle from "@/Components/atoms/SectionTitle.vue";
-import { formatMoney } from "@/utils";
-import { useLocalStorage } from "@vueuse/core";
-import { addMinutes, format } from "date-fns";
-import { onMounted, reactive, computed } from "vue";
-import MoneyPresenter from "../molecules/MoneyPresenter.vue";
-
-const props = defineProps({
-    income: {
-        type: Number
-    },
-    expenses: {
-        type: Number
-    }
-})
-
-
-const image = computed(() => {
-  return state.value.weather.length && state.value.weather[0].icon;
-});
-
-</script>
