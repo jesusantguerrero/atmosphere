f843bbcb86595c8a8168244cdcaa15f048077189
Jesus Guerrero <jesusant.guerrero@gmail.com>
2024-03-29 17:29:02 -0400

    style: add emergency fund widget styles

--- resources/js/Pages/Dashboard/Partials/BudgetFundWidget.vue
+++ resources/js/Pages/Dashboard/Partials/BudgetFundWidget.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-    import { onMounted, ref } from 'vue';
+    import { onMounted, ref, computed } from 'vue';
+    import axios from 'axios';
 
     import WidgetContainer from '@/Components/WidgetContainer.vue';
-    import axios from 'axios';
-import { formatMoney } from '@/utils';
+    import BudgetProgress from '@/domains/budget/components/BudgetProgress.vue';
+    import MoneyPresenter from '@/Components/molecules/MoneyPresenter.vue';
 
     const budgetData = ref({})
 
@@ -12,6 +13,12 @@ import { formatMoney } from '@/utils';
             budgetData.value = data?.at(0);
         });
     }
+
+
+    const fundMetrics = computed(() => ({
+        target: budgetData?.value.monthlyExpense * budgetData?.value.target_times,
+        balance: budgetData.value?.balance ?? 0
+    }))
 
     onMounted(() => {
         fetchChecks()
@@ -23,16 +30,32 @@ import { formatMoney } from '@/utils';
         :message="$t('Emergency Fund Builder')"
     >
         <template #content>
-            <section>
-                <p>
-                    {{ formatMoney(budgetData.balance) }}
-                </p>
-                <p>
-                    {{ formatMoney(budgetData.monthlyExpense) }}
-                </p>
-                <p>
-                    {{ budgetData.total?.toFixed?.(2) }} Months
-                </p>
+            <section class="my-2">
+                <BudgetProgress
+                    class="h-1.5 rounded-sm"
+                    :goal="fundMetrics.target"
+                    :current="budgetData.balance"
+                    :progress-class="['bg-primary', 'bg-secondary/5']"
+                    :show-labels="false"
+                >
+                 <template #before>
+                    <header class="mb-1 font-bold flex justify-between">
+                        <span>
+                            <MoneyPresenter :value="fundMetrics.target" />
+                        </span>
+                        <span>
+                            {{ budgetData.target_times }} Months
+                        </span>
+                    </header>
+                </template>
+
+                    <template v-slot:after="{ progress }">
+                    <div class="flex justify-between w-full mt-1">
+                        <span> <MoneyPresenter :value="fundMetrics.balance" />({{ progress}}%) </span>
+                        <span>{{ budgetData.total?.toFixed?.(2) }} Months</span>
+                    </div>
+                </template>
+                </BudgetProgress>
             </section>
         </template>
     </WidgetContainer>
