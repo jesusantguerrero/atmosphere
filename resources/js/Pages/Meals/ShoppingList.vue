934fb8856d295ce1a3198d83ccc99b54b423cacb
Jesus Guerrero <jesusant.guerrero@gmail.com>
2024-06-17 15:41:12 -0400

    feature: Add shopping list layout

--- resources/js/Pages/Meals/ShoppingList.vue
+++ resources/js/Pages/Meals/ShoppingList.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { router } from "@inertiajs/vue3"
 
 import AppLayout from '@/Components/templates/AppLayout.vue';
+import StatusButtons from "@/Components/molecules/StatusButtons.vue";
+
 import BoardSection from '@/Components/board/BoardSection.vue';
+import BoardItemContainer from '@/Components/board/BoardItemContainer.vue';
 import MealSectionNav from '@/domains/meal/components/MealSectionNav.vue';
 
 interface Plan {
@@ -15,6 +18,7 @@ const props = defineProps<{
     users: Record<string, string>[];
     filters: string[];
     automations: string[];
+    serverSearchOptions: Record<string, any>
 }>()
 
 
@@ -26,25 +30,93 @@ const onSearch = (query: string) => {
     router.replace(`${location.pathname}/${query}`)
 }
 
+console.log(props.chores);
+
+const selectedStage = ref();
+const tasks = computed(() => {
+  return props.chores.at(0).stages.at(0)?.items
+});
+
+const inbox = computed(() => {
+  const inbox = selectedStage.value
+    ? tasks.value.filter((task: Record<string, any>) => task.stage == selectedStage.value)
+    : tasks.value.filter((task: Record<string, any>) => task);
+  return inbox;
+});
+const committed = computed(() => {
+  const inbox = selectedStage.value
+    ? tasks.value.filter((task: Record<string, any>) => task.stage == selectedStage.value)
+    : tasks.value.filter((task: Record<string, any>) => task);
+  return inbox;
+});
+
+const mealStatus = {
+  list: {
+    label: "List",
+    value: "list",
+  },
+  summary: {
+    label: "summary",
+    value: "summary",
+  },
+};
+
+const currentStatus = ref(props.serverSearchOptions?.filters?.is_liked || "summary");
 </script>
 
 <template>
     <AppLayout title="Shopping List">
         <template #header>
-            <MealSectionNav class="h-12" />
+            <MealSectionNav class="h-12" >
+                <template #actions>
+                    <StatusButtons
+                        v-model="currentStatus"
+                        :statuses="mealStatus"
+                    />
+                </template>
+                </MealSectionNav>
       </template>
       <main class="mt-4">
-        <BoardSection
-            v-if="chorePlan"
-            class="w-full pt-12 mx-2 overflow-hidden"
-            :board="chorePlan"
-            :users="users"
-            :automations="automations"
-            :filters="filters"
-            :multi-stage="false"
-            @search="onSearch"
-            resource-name="items"
-        />
+        <template v-if="chorePlan">
+            <BoardSection
+                :layout="currentStatus"
+                class="w-full pt-12 mx-2 overflow-hidden"
+                :board="chorePlan"
+                :users="users"
+                :automations="automations"
+                :filters="filters"
+                :multi-stage="false"
+                @search="onSearch"
+                resource-name="items"
+            />
+            <!-- <BoardItemContainer
+                v-else
+                title="To Do"
+                :allow-add="true"
+                :boards="[]"
+                :tasks="inbox"
+              >
+                <template #empty v-if="committed.length">
+                  <div class="w-full mx-auto prose prose-xl text-center">
+                    <img
+                      src="../../../img/undraw_a_day_at_the_park.svg"
+                      class="w-4/12 mx-auto"
+                    />
+                    <p class="mt-4">All tasks done. take a rest</p>
+                  </div>
+                </template>
+
+                <template #empty v-else>
+                  <div class="w-full mx-auto prose prose-xl text-center">
+                    <img src="../../../img/undraw_empty.svg" class="w-4/12 mx-auto" />
+                    <small class="mt-4 text-gray-400">
+                      Nothing to do. Add new tasks from here or mark in your
+                      <a href="#" @click="openBoards">boards</a> as todo</small
+                    >
+                  </div>
+                </template>
+            </BoardItemContainer> -->
+        </template>
         <WelcomeCard  v-else message="Loger profiles">
             <section class="flex flex-col items-center pb-12 mx-auto">
                 <img src="../LogerProfile/empty-box.svg" class="opacity-50" />
