71120bd3e9a78fb11d9af475ffd7fe08eee1837e
Jesus Guerrero <jesusant.guerrero@gmail.com>
2024-10-15 08:07:03 -0400

    fix: add build frontend to update version

--- app/Console/Commands/UpdateVersion.php
+++ app/Console/Commands/UpdateVersion.php
@@ -3,8 +3,10 @@
 namespace App\Console\Commands;
 
 use ZipArchive;
+use RuntimeException;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
+use Symfony\Component\Process\Process;
 use Illuminate\Support\Facades\Artisan;
 
 class UpdateVersion extends Command
@@ -102,7 +104,44 @@ class UpdateVersion extends Command
             // Copy the file to the correct location in the app
             File::copy($file->getRealPath(), $destination);
         }
-
+        $this->installFront();
         $this->info('Update applied successfully.');
+    }
+
+    public function installFront() {
+        if (file_exists(base_path('pnpm-lock.yaml'))) {
+            $this->runCommands(['pnpm install', 'pnpm run build']);
+        } elseif (file_exists(base_path('yarn.lock'))) {
+            $this->runCommands(['yarn install', 'yarn run build']);
+        } else {
+            $this->runCommands(['npm install', 'npm run build']);
+        }
+
+        $this->line('');
+        $this->components->info('Loger installed successfully.');
+        return true;
+    }
+
+     /**
+     * Run the given commands.
+     *
+     * @param  array  $commands
+     * @return void
+     */
+    protected function runCommands($commands)
+    {
+        $process = Process::fromShellCommandline(implode(' && ', $commands), null, null, null, null);
+
+        if ('\\' !== DIRECTORY_SEPARATOR && file_exists('/dev/tty') && is_readable('/dev/tty')) {
+            try {
+                $process->setTty(true);
+            } catch (RuntimeException $e) {
+                $this->output->writeln('  <bg=yellow;fg=black> WARN </> '.$e->getMessage().PHP_EOL);
+            }
+        }
+
+        $process->run(function ($type, $line) {
+            $this->output->write('    '.$line);
+        });
     }
 }
